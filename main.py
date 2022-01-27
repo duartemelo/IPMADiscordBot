31c0b09ffa153976ee484786e5b8b794476e355a
duartemelo <duarteribeirodemelo@hotmail.com>
2022-01-27 22:05:54 +0000

    Start of project, git ignore

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event()
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    else:
+        await message.channel.send(message)
+
+
+
+client.run()
