932f6658c6b33206609dad0fb504a123e6209502
duartemelo <duarteribeirodemelo@hotmail.com>
2022-01-28 00:26:35 +0000

    Organizing functions

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import utils
 
 client = discord.Client()
 
+commands = ["cities", "weather", "help"]
 
 @client.event
 async def on_ready():
@@ -33,10 +34,8 @@ async def on_message(message):
             await message.channel.send(f"{message.author.mention}\n{message_to_send}")
 
 
-# Return the message to send to the user, depending on the received message from the user
+# Returns the message to send to the user, depending on the received message from the user
 def get_message_to_send(message):
-
-    commands = ["cities", "weather", "help"]
 
     message_to_list = message.split()
 
@@ -46,31 +45,43 @@ def get_message_to_send(message):
     arguments = message_to_list[1:]
     arguments_string = utils.list_to_string(arguments, " ")
 
-
-    #TODO organize
     if command in commands:
-        if command == commands[0]: # cities list
-            message_to_send = f"**Cidades disponíveis:**\n{utils.list_to_string(data_grabbing.get_all_cities(), '; ')}"
-
-        elif command == commands[1]: # weather for a city
-            if message_len > 1:
-                city_code = data_grabbing.get_city_code(arguments_string)
-                if city_code == None:
-                    message_to_send = f"{arguments_string} não existe na lista de cidades. ${commands[0]} para ver a lista."
-                else:
-                    message_to_send = f"{data_grabbing.get_weather(city_code, 0)}"
-            else:
-                message_to_send = "Introduz uma cidade."
-
-        elif command == commands[2]: # help
-            n1 = "\n"
-            message_to_send = f"**Comandos disponíveis:**\n{utils.list_to_string(commands, f';{n1}')}"
-
-        else: # command is in list, but not implemented
-            message_to_send = "Esse comando existe, mas a sua resposta está em desenvolvimento."
+        message_to_send = get_message_to_send_command_handler(command, arguments_string, message_len)
 
     else: # command does not exist
         message_to_send = "Comando não existe."
+
+    return message_to_send
+
+
+# Returns the message to send to the user, handles the command received
+def get_message_to_send_command_handler(command, arguments_string, message_len):
+    if command == commands[0]:  # cities list
+        message_to_send = f"**Cidades disponíveis:**\n{utils.list_to_string(data_grabbing.get_all_cities(), '; ')}"
+
+    elif command == commands[1]:  # weather for a city
+        if message_len > 1:
+            message_to_send = get_message_to_send_weather_for_city(arguments_string)
+        else:
+            message_to_send = "Introduz uma cidade."
+
+    elif command == commands[2]:  # help
+        n1 = "\n"
+        message_to_send = f"**Comandos disponíveis:**\n{utils.list_to_string(commands, f';{n1}')}"
+
+    else:  # command is in list, but not implemented
+        message_to_send = "Esse comando existe, mas a sua resposta está em desenvolvimento."
+
+    return message_to_send
+
+
+# Returns the message to send to the user when he does $weather <city>
+def get_message_to_send_weather_for_city(given_city):
+    city_code = data_grabbing.get_city_code(given_city)
+    if city_code == None:
+        message_to_send = f"{given_city} não existe na lista de cidades. ${commands[0]} para ver a lista."
+    else:
+        message_to_send = f"{data_grabbing.get_weather(city_code, 0)}"
 
     return message_to_send
 
