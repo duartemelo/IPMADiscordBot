406472e2a66f78cb70bcb9a46a2d51564eb6f5d7
duartemelo <duarteribeirodemelo@hotmail.com>
2022-01-31 22:45:41 +0000

    Functions organization refactor, now inside dictionary with lambda functions

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import utils
 
 client = discord.Client()
 
-commands = ["cities", "weather", "help"]
+backslash_n = "\n"
 
 @client.event
 async def on_ready():
@@ -34,52 +34,39 @@ async def on_message(message):
             await message.channel.send(f"{message.author.mention}\n{message_to_send}")
 
 
+commands = {
+    "cities": lambda args: f"**Cidades disponíveis:**\n{utils.list_to_string(data_grabbing.get_all_cities(), '; ')}",
+    "weather": lambda args: get_message_to_send_weather_for_city(args) if args != "" else "**Insere uma cidade**",
+    "help": lambda args: f"**Comandos disponíveis:**\n{utils.list_to_string(commands, f';{backslash_n}')}"
+}
+
+
 # Returns the message to send to the user, depending on the received message from the user
 def get_message_to_send(message):
 
     message_to_list = message.split()
-
-    message_len = len(message_to_list)
 
     command = message_to_list[0]
     arguments = message_to_list[1:]
     arguments_string = utils.list_to_string(arguments, " ")
 
     if command in commands:
-        message_to_send = get_message_to_send_command_handler(command, arguments_string, message_len)
+        func = commands[command]
+        message_to_send = func(arguments_string)
 
     else: # command does not exist
-        message_to_send = "Comando não existe."
-
-    return message_to_send
-
-
-# Returns the message to send to the user, handles the command received
-def get_message_to_send_command_handler(command, arguments_string, message_len):
-    if command == commands[0]:  # cities list
-        message_to_send = f"**Cidades disponíveis:**\n{utils.list_to_string(data_grabbing.get_all_cities(), '; ')}"
-
-    elif command == commands[1]:  # weather for a city
-        if message_len > 1:
-            message_to_send = get_message_to_send_weather_for_city(arguments_string)
-        else:
-            message_to_send = "Introduz uma cidade."
-
-    elif command == commands[2]:  # help
-        n1 = "\n"
-        message_to_send = f"**Comandos disponíveis:**\n{utils.list_to_string(commands, f';{n1}')}"
-
-    else:  # command is in list, but not implemented
-        message_to_send = "Esse comando existe, mas a sua resposta está em desenvolvimento."
+        message_to_send = "Esse comando não existe."
 
     return message_to_send
 
 
 # Returns the message to send to the user when he does $weather <city>
 def get_message_to_send_weather_for_city(given_city):
+
     city_code = data_grabbing.get_city_code(given_city)
-    if city_code == None:
-        message_to_send = f"{given_city} não existe na lista de cidades. ${commands[0]} para ver a lista."
+    if city_code is None:
+        keys_list = list(commands)
+        message_to_send = f"{given_city} não existe na lista de cidades. ${keys_list[0]} para ver a lista." #fix, dynamic help command
     else:
         message_to_send = f"{data_grabbing.get_weather(city_code, 0)}"
 
