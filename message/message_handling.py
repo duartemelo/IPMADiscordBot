b30ca4c93f79c49329202101679a5b3a57946585
duartemelo <duarteribeirodemelo@hotmail.com>
2022-02-20 00:50:02 +0000

    Minor documentation

--- message/message_handling.py
+++ message/message_handling.py
@@ -12,7 +12,7 @@ from database_stuff.select_time import select_time
 backslash_n = "\n"  # created because of the impossibility of using \n inside f strings
 
 # Commands templates, basically how a command should be used
-# TODO: add description!
+# TODO: add description (?)
 commands_templates = {
     "cities": "$cities",
     "weather": "$weather <city> <day (from 0 to 4)>",
